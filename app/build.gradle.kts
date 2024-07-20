95359068d39219db300d2f6e980187df6e992bbd
miebrahim <miebrahim@elm.sa>
2024-07-20 21:25:47 +0300

    Lect-8_adding_build_dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,19 +51,24 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(Dependencies.ANDROIDX_CORE)
+    implementation(Dependencies.ANDROIDX_LIFECYCLE_RUNTIME_KTX)
+    implementation(Dependencies.ANDROIDX_ACTIVITY_COMPOSE)
+    implementation(Dependencies.ANDROIDX_UI)
+    implementation(Dependencies.ANDROIDX_UI_GRAPHICS)
+    implementation(Dependencies.ANDROIDX_UI_TOOLING_PREVIEW)
+    implementation(Dependencies.ANDROIDX_MATERIAL3)
+
+
     testImplementation(libs.junit)
+
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
---
+++ buildSrc/src/main/kotlin/DependenciesVersions.kt
@@ -0,0 +1,11 @@
+object DependenciesVersions {
+    const val CORE_KTX = "1.10.1"
+    const val LIFE_CYCLE_RUNTIME_KTX = "2.6.1"
+    const val KOTLIN = "1.9.0"
+    const val JUNIT = "4.13.2"
+    const val JUNIT_VERSION = "1.1.5"
+    const val ESPRESSO_CORE = "3.5.1"
+    const val ACTIVITY_COMPOSE = "1.8.0"
+    const val COMPOSE_UI = "1.6.8"
+    const val MATERIAL_3 = "1.2.1"
+}
