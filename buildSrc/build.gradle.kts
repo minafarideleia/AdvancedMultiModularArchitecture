b5e9fa77fc585c67df8089549051813b537d38e5
miebrahim <miebrahim@elm.sa>
2024-07-22 00:58:20 +0300

    Lect-11---Add-configuration-in-build-gradle-of-build-src

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,11 @@ plugins {
 }
 
 repositories {
+    google()
     mavenCentral()
+}
+
+dependencies {
+    api(kotlin("gradle-plugin:1.9.0"))
+    implementation("com.android.tools.build:gradle:8.1.4")
 }
