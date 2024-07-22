29923077ccf2b482d5cae327760f2202a6d52239
miebrahim <miebrahim@elm.sa>
2024-07-23 02:09:04 +0300

    Lect-15-Add-Google,-Huawei,-Client-and-Driver-Flavors

--- buildSrc/src/main/kotlin/BuildFlavor.kt
+++ buildSrc/src/main/kotlin/BuildFlavor.kt
@@ -14,4 +14,69 @@ sealed class BuildFlavor(val name: String) {
     ) : LibraryProductFlavor
 
 
+    object Google : BuildFlavor(FlavorTypes.GOOGLE){
+        override fun create(namedDomainObjectContainer: NamedDomainObjectContainer<ApplicationProductFlavor>): ApplicationProductFlavor {
+            return namedDomainObjectContainer.create(name){
+                dimension = BuildDimensions.STORE
+                applicationIdSuffix = ".$name"
+                versionNameSuffix = "-$name"
+            }
+        }
+
+        override fun createLibrary(namedDomainObjectContainer: NamedDomainObjectContainer<LibraryProductFlavor>): LibraryProductFlavor {
+            return namedDomainObjectContainer.create(name){
+                dimension = BuildDimensions.STORE
+            }
+        }
+    }
+
+    object Huawei : BuildFlavor(FlavorTypes.HUAWEI){
+        override fun create(namedDomainObjectContainer: NamedDomainObjectContainer<ApplicationProductFlavor>): ApplicationProductFlavor {
+            return namedDomainObjectContainer.create(name){
+                dimension = BuildDimensions.STORE
+                applicationIdSuffix = ".$name"
+                versionNameSuffix = "-$name"
+            }
+        }
+
+        override fun createLibrary(namedDomainObjectContainer: NamedDomainObjectContainer<LibraryProductFlavor>): LibraryProductFlavor {
+            return namedDomainObjectContainer.create(name){
+                dimension = BuildDimensions.STORE
+            }
+        }
+    }
+
+    object Driver : BuildFlavor(FlavorTypes.DRIVER){
+        override fun create(namedDomainObjectContainer: NamedDomainObjectContainer<ApplicationProductFlavor>): ApplicationProductFlavor {
+            return namedDomainObjectContainer.create(name){
+                dimension = BuildDimensions.APP
+                applicationIdSuffix = ".$name"
+                versionNameSuffix = "-$name"
+            }
+        }
+
+        override fun createLibrary(namedDomainObjectContainer: NamedDomainObjectContainer<LibraryProductFlavor>): LibraryProductFlavor {
+            return namedDomainObjectContainer.create(name){
+                dimension = BuildDimensions.APP
+            }
+        }
+    }
+
+    object Client : BuildFlavor(FlavorTypes.CLIENT){
+        override fun create(namedDomainObjectContainer: NamedDomainObjectContainer<ApplicationProductFlavor>): ApplicationProductFlavor {
+            return namedDomainObjectContainer.create(name){
+                dimension = BuildDimensions.APP
+                applicationIdSuffix = ".$name"
+                versionNameSuffix = "-$name"
+            }
+        }
+
+        override fun createLibrary(namedDomainObjectContainer: NamedDomainObjectContainer<LibraryProductFlavor>): LibraryProductFlavor {
+            return namedDomainObjectContainer.create(name){
+                dimension = BuildDimensions.APP
+            }
+        }
+    }
+
+
 }
