ead440866119b967b1f529d9192abb2b0e603086
miebrahim <miebrahim@elm.sa>
2024-07-26 15:31:21 +0300

    Lect-21-Test-Siging-config

--- buildSrc/src/main/kotlin/BuildSigning.kt
+++ buildSrc/src/main/kotlin/BuildSigning.kt
@@ -1,5 +1,6 @@
 import com.android.build.api.dsl.ApkSigningConfig
 import org.gradle.api.NamedDomainObjectContainer
+import org.gradle.api.Project
 import java.io.File
 
 sealed class BuildSigning(val name: String) {
@@ -7,39 +8,39 @@ sealed class BuildSigning(val name: String) {
     abstract fun create(namedDomainObjectContainer: NamedDomainObjectContainer<out ApkSigningConfig>)
 
 
-    object Release : BuildSigning(SigningTypes.RELEASE) {
+    class Release(private val project: Project) : BuildSigning(SigningTypes.RELEASE) {
         override fun create(namedDomainObjectContainer: NamedDomainObjectContainer<out ApkSigningConfig>) {
             namedDomainObjectContainer.create(name) {
-                storeFile = File("path/release/myKeyStore.jks")
-                storePassword = "release_store_password"
-                keyAlias = "release_key_alias"
-                keyPassword = "release_key_password"
+                storeFile = File(project.getLocalProperty("release_key.store"))
+                storePassword = project.getLocalProperty("release_store.password")
+                keyAlias = project.getLocalProperty("release_key.alias")
+                keyPassword =project.getLocalProperty("release_key.password")
                 enableV1Signing = true
                 enableV2Signing = true
             }
         }
     }
 
-    object ReleaseExternalQa : BuildSigning(SigningTypes.RELEASE_EXTERNAL_QA) {
+    class ReleaseExternalQa(private  val project: Project) : BuildSigning(SigningTypes.RELEASE_EXTERNAL_QA) {
         override fun create(namedDomainObjectContainer: NamedDomainObjectContainer<out ApkSigningConfig>) {
             namedDomainObjectContainer.create(name) {
-                storeFile = File("path/releaseExternalQA/myKeyStore.jks")
-                storePassword = "release_store_password"
-                keyAlias = "release_key_alias"
-                keyPassword = "release_key_password"
+                storeFile = File(project.getLocalProperty("qa_key.store"))
+                storePassword = project.getLocalProperty("qa_store.password")
+                keyAlias = project.getLocalProperty("qa_key.alias")
+                keyPassword =project.getLocalProperty("qa_key.password")
                 enableV1Signing = true
                 enableV2Signing = true
             }
         }
     }
 
-    object Debug : BuildSigning(SigningTypes.DEBUG) {
+    class Debug(private val project: Project) : BuildSigning(SigningTypes.DEBUG) {
         override fun create(namedDomainObjectContainer: NamedDomainObjectContainer<out ApkSigningConfig>) {
             namedDomainObjectContainer.getByName(name) {
-                storeFile = File("path/debug/myKeyStore.jks")
-                storePassword = "release_store_password"
-                keyAlias = "release_key_alias"
-                keyPassword = "release_key_password"
+                storeFile = File(project.rootProject.rootDir,"debug.keystore")
+                storePassword = "android"
+                keyAlias = "androiddebugkey"
+                keyPassword = "android"
                 enableV1Signing = true
                 enableV2Signing = true
             }
--- buildSrc/src/main/kotlin/TestDependencies.kt
+++ buildSrc/src/main/kotlin/TestDependencies.kt
@@ -1,5 +1,5 @@
 object TestDependencies {
-    const val ANDROIDX_JUNIT = "androidx.test.ext:junit:${DependenciesVersions.JUNIT}"
+    const val ANDROIDX_JUNIT = "androidx.test.ext:junit:${DependenciesVersions.JUNIT_VERSION}"
     const val ANDROIDX_ESPRESSO_CORE =
         "androidx.test.espresso:espresso-core:${DependenciesVersions.ESPRESSO_CORE}"
     const val ANDROIDX_COMPOSE_UI_TEST =
