7017bff2772cf43157a15299260d5421fa9df58e
miebrahim <miebrahim@elm.sa>
2024-07-29 19:25:45 +0300

    Lect-23-How-to-add-build-parameters-to-build-config

---
+++ buildSrc/src/main/kotlin/BuildVariables.kt
@@ -0,0 +1,6 @@
+object BuildVariables {
+    const val BASE_URL = "BASE_URL"
+    const val CAN_CLEAR_CACHE = "CAN_CLEAR_CACHE"
+    const val DB_VERSION = "DB_VERSION"
+    const val MAP_KEY = "MAP_KEY"
+}
--- buildSrc/src/main/kotlin/Extensions.kt
+++ buildSrc/src/main/kotlin/Extensions.kt
@@ -1,16 +1,29 @@
+import com.android.build.api.dsl.ApplicationBuildType
 import org.gradle.api.Project
 import java.util.Properties
 
 private const val LOCAL_PROPERTIES_FILE_NAME = "dev_credentials.properties"
 
-fun Project.getLocalProperty(propertyName:String) : String{
+fun Project.getLocalProperty(propertyName: String): String {
     val localProperties = Properties().apply {
         val localPropertiesFile =
             project.rootProject.file(LOCAL_PROPERTIES_FILE_NAME)
-        if(localPropertiesFile.exists()){
+        if (localPropertiesFile.exists()) {
             load(localPropertiesFile.inputStream())
         }
     }
-    return localProperties.getProperty(propertyName) ?: throw
-    NoSuchFieldException("Property not found: $propertyName")
+    return localProperties.getProperty(propertyName)
+        ?: throw NoSuchFieldException("Property not found: $propertyName")
+}
+
+fun ApplicationBuildType.buildConfigStringField(name: String, value: String) {
+    this.buildConfigField("String", name, value)
+}
+
+fun ApplicationBuildType.buildConfigIntField(name: String, value: String) {
+    this.buildConfigField("int", name, value)
+}
+
+fun ApplicationBuildType.buildConfigBooleanField(name: String, value: String) {
+    this.buildConfigField("boolean", name, value)
 }
