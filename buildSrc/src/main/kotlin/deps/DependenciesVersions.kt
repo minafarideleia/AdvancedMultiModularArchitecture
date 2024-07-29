226aabd2d920632d2dbece63a2fa2decde75f3b4
miebrahim <miebrahim@elm.sa>
2024-07-30 00:59:55 +0300

    Lect-27-add-more-dependencies

--- buildSrc/src/main/kotlin/deps/DependenciesVersions.kt
+++ buildSrc/src/main/kotlin/deps/DependenciesVersions.kt
@@ -10,4 +10,10 @@ object DependenciesVersions {
     const val ACTIVITY_COMPOSE = "1.8.0"
     const val COMPOSE_UI = "1.6.8"
     const val MATERIAL_3 = "1.2.1"
+
+    const val RETROFIT = "2.11.0"
+    const val RETROFIT_COROUTINE_ADAPTER_VERSION = "0.9.2"
+    const val OKHTTP = "5.0.0-alpha.2"
+    const val HILT = "2.44"
+    const val ROOM = "2.6.1"
 }
