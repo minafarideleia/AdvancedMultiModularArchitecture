4ce7abb30ddf51bab1c0814064244c213f0f5ef7
miebrahim <miebrahim@elm.sa>
2024-07-31 22:54:33 +0300

    Lect-29-Make-deps-shared-over-modules

--- buildSrc/src/main/kotlin/deps/Dependencies.kt
+++ buildSrc/src/main/kotlin/deps/Dependencies.kt
@@ -7,7 +7,29 @@ object Dependencies {
     const val ANDROIDX_ACTIVITY_COMPOSE =
         "androidx.activity:activity-compose:${DependenciesVersions.ACTIVITY_COMPOSE}"
     const val ANDROIDX_UI = "androidx.compose.ui:ui:${DependenciesVersions.COMPOSE_UI}"
-    const val ANDROIDX_UI_GRAPHICS = "androidx.compose.ui:ui-graphics:${DependenciesVersions.COMPOSE_UI}"
-    const val ANDROIDX_UI_TOOLING_PREVIEW = "androidx.compose.ui:ui-tooling-preview:${DependenciesVersions.COMPOSE_UI}"
-     const val ANDROIDX_MATERIAL3= "androidx.compose.material3:material3:${DependenciesVersions.MATERIAL_3}"
+    const val ANDROIDX_UI_GRAPHICS =
+        "androidx.compose.ui:ui-graphics:${DependenciesVersions.COMPOSE_UI}"
+    const val ANDROIDX_UI_TOOLING_PREVIEW =
+        "androidx.compose.ui:ui-tooling-preview:${DependenciesVersions.COMPOSE_UI}"
+    const val ANDROIDX_MATERIAL3 =
+        "androidx.compose.material3:material3:${DependenciesVersions.MATERIAL_3}"
+
+    const val hiltAndroid = "com.google.dagger:hilt-android:${DependenciesVersions.HILT}"
+    const val hiltCompiler = "com.google.dagger:hilt-compiler:${DependenciesVersions.HILT}"
+    const val hiltAgp = "com.google.dagger:hilt-android-gradle-plugin:${DependenciesVersions.HILT}"
+
+    const val retrofit = "com.squareup.retrofit2:retrofit:${DependenciesVersions.RETROFIT}"
+    const val retrofitConverterGson =
+        "com.squareup.retrofit2:converter-gson:${DependenciesVersions.RETROFIT}"
+    const val retrofitKotlinCoroutinesAdapter =
+        "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${DependenciesVersions.RETROFIT_COROUTINE_ADAPTER_VERSION}"
+
+    const val okHttp = "com.squareup.okhttp3:okhttp:${DependenciesVersions.OKHTTP}"
+    const val okHttpLoggingInterceptor =
+        "com.squareup.okhttp3:logging-interceptor:${DependenciesVersions.OKHTTP}"
+
+    const val roomRuntime = "androidx.room:room-runtime:${DependenciesVersions.ROOM}"
+    const val roomCompiler = "androidx.room:room-compiler:${DependenciesVersions.ROOM}"
+    const val roomKtx = "androidx.room:room-ktx:${DependenciesVersions.ROOM}"
+
 }
