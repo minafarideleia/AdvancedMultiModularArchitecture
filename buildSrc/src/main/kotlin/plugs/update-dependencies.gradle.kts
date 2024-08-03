534297441af0611f800db08b24bb69c5ff4c582c
miebrahim <miebrahim@elm.sa>
2024-08-03 14:32:34 +0300

    Lect-45-Generate-Updating-Build-Versions-Report

--- buildSrc/src/main/kotlin/plugs/update-dependencies.gradle.kts
+++ buildSrc/src/main/kotlin/plugs/update-dependencies.gradle.kts
@@ -1,13 +1,33 @@
 import com.github.benmanes.gradle.versions.VersionsPlugin
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import java.util.Locale
 
 apply<VersionsPlugin>()
 
 tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
+// exclude non stable versions
+    resolutionStrategy {
+        componentSelection {
+            all {
+                if (isNonStable(candidate.version) && !isNonStable(currentVersion)) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
 
     // optional parameters
     checkForGradleUpdate = true
     outputFormatter = "html"
     outputDir = "${project.rootDir}/reports/dependencyUpdates"
     reportfileName = "report"
+}
+
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword =
+        listOf("RELEASE", "FINAL", "GA").any { version.uppercase(Locale.ROOT).contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
