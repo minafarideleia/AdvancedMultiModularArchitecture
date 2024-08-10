9b0f48d43a7efef23220e20c70bc1499d9c57aee
miebrahim <miebrahim@elm.sa>
2024-08-11 02:00:49 +0300

    Lect-71-Defining-Error-Handling-Models-with-Data-Classes

---
+++ core/data/src/main/java/com/minafarid/data/model/ErrorMessage.kt
@@ -0,0 +1,6 @@
+package com.minafarid.data.model
+
+data class ErrorMessage(
+    val code: Int,
+    val message: String
+)
---
+++ core/data/src/main/java/com/minafarid/data/response/ErrorResponse.kt
@@ -0,0 +1,12 @@
+package com.minafarid.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ErrorResponse(
+    @SerializedName("errorCode")
+    val errorCode: String,
+    @SerializedName("errorMessage")
+    val errorMessage: String,
+    @SerializedName("errorFieldList")
+    val errorFieldList: List<String>,
+    )
