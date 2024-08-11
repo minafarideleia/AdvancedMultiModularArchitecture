9e761eb57502b07dacf9112615c0f84158ddfd1f
miebrahim <miebrahim@elm.sa>
2024-08-12 02:08:19 +0300

    Lect-76-Dynamic-Response-Processing-with-performRequest-in-Network-Data-Source

--- core/data/src/main/java/com/minafarid/data/source/NetworkDataSource.kt
+++ core/data/src/main/java/com/minafarid/data/source/NetworkDataSource.kt
@@ -2,10 +2,36 @@ package com.minafarid.data.source
 
 import com.google.gson.Gson
 import com.minafarid.data.connectivity.NetworkMonitorInterface
+import com.minafarid.data.response.ErrorResponse
+import com.minafarid.data.result.OutCome
+import okhttp3.Headers
+import retrofit2.Response
 
 class NetworkDataSource<SERVICE>(
     private val service: SERVICE,
     private val gson: Gson,
     private val networkMonitorInterface: NetworkMonitorInterface,
     private val userIdProvider: () -> String
-)
+) {
+
+    suspend fun <R, T> performRequest(
+        request: suspend SERVICE.(String) -> Response<R>,
+        onSuccess: suspend (R, Headers) -> OutCome<T> = { _, _ -> OutCome.empty() },
+        onRedirect: suspend (String, Int) -> OutCome<T> = { _, _ -> OutCome.empty() },
+        onEmpty: suspend () -> OutCome<T> = { OutCome.empty() },
+        onError: suspend (ErrorResponse, Int) -> OutCome<T> = { errorResponse, code ->
+            OutCome.error(
+                errorResponse.toDomain()
+
+            )
+        },
+
+
+        )
+
+            : OutCome<T> {
+
+    }
+
+
+}
