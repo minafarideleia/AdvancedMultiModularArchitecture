9a06c726d90650c3981f050057cfb117519e6318
miebrahim <miebrahim@elm.sa>
2024-08-17 10:46:28 +0300

    Lect-82-Add-Implementation-of-Remote-Implementer

--- features/login/src/main/java/com/minafarid/login/data/source/LoginRemoteImplementer.kt
+++ features/login/src/main/java/com/minafarid/login/data/source/LoginRemoteImplementer.kt
@@ -5,14 +5,18 @@ import com.minafarid.data.result.OutCome
 import com.minafarid.data.source.NetworkDataSource
 import com.minafarid.login.data.requests.LoginRequestBody
 import com.minafarid.login.data.service.LoginService
-import com.minafarid.login.domain.User
+import com.minafarid.login.domain.mapper.LoginMapper
+import com.minafarid.login.domain.model.User
 
-class LoginRemoteImplementer(private val networkDataSource: NetworkDataSource<LoginService>) :
+class LoginRemoteImplementer(
+    private val networkDataSource: NetworkDataSource<LoginService>,
+    private val loginMapper: LoginMapper
+) :
     LoginRemote {
     override suspend fun login(loginRequestBody: LoginRequestBody): OutCome<User> {
         return networkDataSource.performRequest(
             request = { login(loginRequestBody).await() },
-            onSuccess = { response, headers -> OutCome.success() },
+            onSuccess = { response, _ -> OutCome.success(loginMapper.toDomain(response)) },
             onError = { errorResponse, code -> OutCome.error(errorResponse.toDomain(code)) }
         )
     }
