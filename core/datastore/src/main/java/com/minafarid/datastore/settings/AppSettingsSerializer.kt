31467c05aeac74bc9ddb54c18e202d06b169553c
miebrahim <miebrahim@elm.sa>
2024-08-23 10:18:18 +0300

    Lect-90-Handle-Serialzer-Function-for-App-Settings

--- core/datastore/src/main/java/com/minafarid/datastore/settings/AppSettingsSerializer.kt
+++ core/datastore/src/main/java/com/minafarid/datastore/settings/AppSettingsSerializer.kt
@@ -7,28 +7,28 @@ import java.io.InputStream
 import java.io.OutputStream
 
 class AppSettingsSerializer : Serializer<AppSettings> {
-    override val defaultValue: AppSettings
-        get() = AppSettings()
+  override val defaultValue: AppSettings
+    get() = AppSettings()
 
-    override suspend fun readFrom(input: InputStream): AppSettings {
-        return try {
-            Json.decodeFromString(
-                deserializer = AppSettings.serializer(),
-                string = input.readBytes().decodeToString(),
+  override suspend fun readFrom(input: InputStream): AppSettings {
+    return try {
+      Json.decodeFromString(
+        deserializer = AppSettings.serializer(),
+        string = input.readBytes().decodeToString(),
 
-                )
-        } catch (e: SerializationException) {
-            e.printStackTrace()
-            defaultValue
-        }
+      )
+    } catch (e: SerializationException) {
+      e.printStackTrace()
+      defaultValue
     }
+  }
 
-    override suspend fun writeTo(t: AppSettings, output: OutputStream) {
-        output.write(
-            Json.encodeToString(
-                serializer = AppSettings.serializer(),
-                value = t
-            ).encodeToByteArray()
-        )
-    }
+  override suspend fun writeTo(t: AppSettings, output: OutputStream) {
+    output.write(
+      Json.encodeToString(
+        serializer = AppSettings.serializer(),
+        value = t,
+      ).encodeToByteArray(),
+    )
+  }
 }
