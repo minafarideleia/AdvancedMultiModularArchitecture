a2798b97c05085c57ecfb6f3f286e1831aecb863
miebrahim <miebrahim@elm.sa>
2024-08-24 02:44:50 +0300

    Lect-99-Create-App-Preferences-Serializer

--- core/protodatastore/src/main/java/com/minafarid/protodatastore/serialzer/PreferencesSerializer.kt
+++ core/protodatastore/src/main/java/com/minafarid/protodatastore/serialzer/PreferencesSerializer.kt
@@ -1,28 +1,28 @@
-package com.minafarid.protodatastore
+package com.minafarid.protodatastore.serialzer
 
 import androidx.datastore.core.Serializer
 import com.google.protobuf.InvalidProtocolBufferException
-import com.minafarid.proto.Session
+import com.minafarid.proto.Preferences
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.InputStream
 import java.io.OutputStream
 
-object SessionSerializer : Serializer<Session> {
-    override val defaultValue: Session
-        get() = Session.getDefaultInstance()
+object PreferencesSerializer : Serializer<Preferences> {
+    override val defaultValue: Preferences
+        get() = Preferences.getDefaultInstance()
 
-    override suspend fun readFrom(input: InputStream): Session =
+    override suspend fun readFrom(input: InputStream): Preferences =
         withContext(Dispatchers.IO) {
             return@withContext try {
-                Session.parseFrom(input)
+                Preferences.parseFrom(input)
             } catch (e: InvalidProtocolBufferException) {
                 e.printStackTrace()
                 defaultValue
             }
         }
 
-    override suspend fun writeTo(t: Session, output: OutputStream) {
+    override suspend fun writeTo(t: Preferences, output: OutputStream) {
         withContext(Dispatchers.IO) {
             t.writeTo(output)
         }
