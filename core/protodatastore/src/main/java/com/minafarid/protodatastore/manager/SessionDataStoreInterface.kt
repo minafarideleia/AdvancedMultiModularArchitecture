b73a362f4333b860c18f381bc94b1454aaf00981
miebrahim <miebrahim@elm.sa>
2024-08-24 03:55:01 +0300

    Lect-103-Create-Session-DataStore-Manager

---
+++ core/protodatastore/src/main/java/com/minafarid/protodatastore/manager/SessionDataStoreInterface.kt
@@ -0,0 +1,29 @@
+package com.minafarid.protodatastore.manager
+
+import com.minafarid.proto.Session
+import kotlinx.coroutines.flow.Flow
+
+
+interface SessionDataStoreInterface {
+    // setter functions
+    // setter functions
+
+    suspend fun setAccessToken(accessToken: String)
+    suspend fun setRefreshToken(refreshToken: String)
+    suspend fun setUserIdToken(userId: String)
+
+    suspend fun setSession(accessToken: String, refreshToken: String, userId: String)
+
+    // getters
+
+    suspend fun getAccessToken(): String
+    fun getAccessTokenFlow(): Flow<String>
+    suspend fun getRefreshToken(): String
+    fun getRefreshTokenFlow(): Flow<String>
+    suspend fun getUserId(): String
+    fun getUserIdFlow(): Flow<String>
+
+    suspend fun getSession(): Session
+    fun getSessionFlow(): Flow<Session>
+
+}
