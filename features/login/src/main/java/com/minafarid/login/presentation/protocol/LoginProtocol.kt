1e3fdcf66740e3b907402b3ceda5b9bec3ffdcdc
miebrahim <miebrahim@elm.sa>
2024-08-30 01:24:22 +0300

    Lect-125-Handling-Login-State-and-Validation-with-LoginViewState

--- features/login/src/main/java/com/minafarid/login/presentation/protocol/LoginProtocol.kt
+++ features/login/src/main/java/com/minafarid/login/presentation/protocol/LoginProtocol.kt
@@ -1,6 +1,7 @@
 package com.minafarid.login.presentation.protocol
 
 import com.minafarid.domain.model.ErrorMessage
+import com.minafarid.login.presentation.error.LoginError
 
 sealed class LoginInput {
     data class UserNameUpdated(val username: String) : LoginInput()
@@ -14,3 +15,21 @@ sealed class LoginOutput {
     data object NavigateToRegister : LoginOutput()
     data class ShowError(val errorMessage: ErrorMessage) : LoginOutput()
 }
+
+data class LoginViewState(
+    val userName: String = "",
+    val password: String = "",
+    val isLoginButtonEnabled: Boolean = false,
+    val userNameError: LoginError = LoginError.NoEntry,
+    val passwordError: LoginError = LoginError.NoEntry,
+) {
+    fun showPasswordError() =
+        passwordError != LoginError.NoError && passwordError != LoginError.NoEntry
+
+    fun showUsernameError() =
+        userNameError != LoginError.NoError && userNameError != LoginError.NoEntry
+}
+
+
+
+
