af68c26b2af57976d54cca50d02590bea83213f3
miebrahim <miebrahim@elm.sa>
2024-08-30 02:54:54 +0300

    Lect-129-Integrating-ViewModel-and-Compose-for-Login-Screen

--- features/login/src/main/java/com/minafarid/login/presentation/view/LoginView.kt
+++ features/login/src/main/java/com/minafarid/login/presentation/view/LoginView.kt
@@ -2,10 +2,13 @@ package com.minafarid.login.presentation.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextButton
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,43 +19,82 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import com.minafarid.login.R
+import com.minafarid.login.presentation.protocol.LoginInput
+import com.minafarid.login.presentation.protocol.LoginViewState
 import com.minafarid.login.presentation.viewmodel.LoginViewModel
 
 @Composable
-fun LoginScreen(loginViewModel: LoginViewModel) {
-  var username by remember { mutableStateOf("") }
-  var password by remember { mutableStateOf("") }
+fun LoginScreen(loginViewState: LoginViewState, loginViewModel: LoginViewModel) {
 
-  Surface(modifier = Modifier.fillMaxSize()) {
-    Column(
-      modifier = Modifier.padding(16.dp),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center,
-    ) {
-      OutlinedTextField(
-        value = username,
-        onValueChange = { username = it },
-        label = { Text(text = "Username") },
-        modifier = Modifier
-          .fillMaxWidth()
-          .padding(8.dp),
-      )
-      OutlinedTextField(
-        value = password,
-        onValueChange = { password = it },
-        label = { Text(text = "Password") },
-        modifier = Modifier
-          .fillMaxWidth()
-          .padding(8.dp),
-      )
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            CustomTextField(
+                label = stringResource(id = R.string.username_label),
+                value = loginViewState.userName,
+                errorText = stringResource(id = loginViewState.userNameError.getErrorMessage()),
+                showError = loginViewState.showUsernameError()
+            ) { userName ->
+                loginViewModel.setInput(LoginInput.UserNameUpdated(userName))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            CustomTextField(
+                label = stringResource(id = R.string.password_label),
+                value = loginViewState.password,
+                errorText = stringResource(id = loginViewState.passwordError.getErrorMessage()),
+                showError = loginViewState.showPasswordError()
+            ) { password ->
+                loginViewModel.setInput(LoginInput.PasswordUpdated(password))
+            }
+            Spacer(modifier = Modifier.height(16.dp))
 
-      Button(
-        modifier = Modifier.fillMaxWidth(),
-        onClick = { loginViewModel.login(username, password) },
-      ) {
-        Text(text = "Login")
-      }
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { loginViewModel.login() },
+            ) {
+                Text(text = "Login")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            TextButton(onClick = { loginViewModel.setInput(LoginInput.RegisterButtonClicked) }) {
+                Text(text = "Sign up Now!")
+            }
+        }
     }
-  }
+
+}
+
+
+@Composable
+fun CustomTextField(
+    label: String,
+    value: String,
+    showError: Boolean,
+    errorText: String,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onChanged: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = { onChanged(it) },
+        label = { Text(text = label) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        isError = showError,
+        visualTransformation = visualTransformation
+    )
+    if (showError) {
+        Text(
+            text = errorText, color = Color.Red,
+            modifier = Modifier.padding(all = 8.dp)
+        )
+    }
 }
