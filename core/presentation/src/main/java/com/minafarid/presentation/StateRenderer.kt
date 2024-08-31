adc84e0ada2e718468090444a6138d4e6bbb0490
miebrahim <miebrahim@elm.sa>
2024-09-01 01:47:49 +0300

    Lect-139-Unified-UI-States-Content,-Loading,-Error,-and-Success-States

---
+++ core/presentation/src/main/java/com/minafarid/presentation/StateRenderer.kt
@@ -0,0 +1,25 @@
+package com.minafarid.presentation
+
+sealed class StateRenderer <out S,O> {
+    // S for ViewState and O for Output
+
+    // Content State
+
+
+    // Loading States
+    // 1- Popup Loading State
+
+    // 2- Full Screen Loading State
+
+
+    // Error States
+    // 1- Popup Error State
+
+    // 2- Full Screen Error State
+
+
+    // Empty State
+
+
+    // Success State
+}
