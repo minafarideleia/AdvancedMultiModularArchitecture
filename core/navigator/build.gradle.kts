2cb186f566f221aa3c71fbdb3eadba17966603d0
miebrahim <miebrahim@elm.sa>
2024-09-05 01:04:00 +0300

    Lect-151-Create-Navigation-Module

---
+++ core/navigator/build.gradle.kts
@@ -0,0 +1,23 @@
+import deps.androidx
+import deps.hilt
+import deps.testDebugDeps
+import deps.testDeps
+import deps.testImplDeps
+import plugs.SharedLibraryGradlePlugin
+
+plugins {
+    id(plugs.BuildPlugins.ANDROID_LIBRARY)
+}
+apply<SharedLibraryGradlePlugin>()
+
+android {
+    namespace = "com.minafarid.navigator"
+}
+
+dependencies {
+    androidx()
+    hilt()
+    testDeps()
+    testImplDeps()
+    testDebugDeps()
+}
