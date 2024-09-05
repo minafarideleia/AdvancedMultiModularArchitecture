30bc6a2cc52e0974e2c50009bb2bb3bd48437578
miebrahim <miebrahim@elm.sa>
2024-09-06 01:04:05 +0300

    Lect-157-Create-Sign-Up-Module-and-UI

---
+++ features/signup/build.gradle.kts
@@ -0,0 +1,43 @@
+import deps.DependenciesVersions
+import deps.androidx
+import deps.dataModule
+import deps.domainModule
+import deps.hilt
+import deps.presentationModule
+import deps.retrofit
+import deps.room
+import deps.testDebugDeps
+import deps.testDeps
+import deps.testImplDeps
+import plugs.SharedLibraryGradlePlugin
+
+plugins {
+    id(plugs.BuildPlugins.ANDROID_LIBRARY)
+    id(plugs.BuildPlugins.HILT) version deps.DependenciesVersions.HILT
+}
+apply<SharedLibraryGradlePlugin>()
+
+android {
+    namespace = "com.minafarid.signup"
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = DependenciesVersions.KOTLIN_COMPILER
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    androidx()
+    retrofit()
+    dataModule()
+    domainModule()
+    presentationModule()
+    hilt()
+    room()
+    testDeps()
+    testImplDeps()
+    testDebugDeps()
+}
