5e7f276101a91bb268c94e94875a61cf340bdb11
miebrahim <miebrahim@elm.sa>
2024-09-06 03:50:05 +0300

    Lect-162-Implement-Navigation-with-Composable-Destinations

--- core/navigator/src/main/java/com/minafarid/navigator/desinations/LoginDestination.kt
+++ core/navigator/src/main/java/com/minafarid/navigator/desinations/LoginDestination.kt
@@ -4,7 +4,7 @@ import androidx.navigation.NamedNavArgument
 const val LOGIN_ROUTE = "LoginRoute"
 
 class LoginDestination : NavigationDestination {
-    override fun destination(): String =  Screens.LoginScreenRoute.route
+    override fun route(): String =  Screens.LoginScreenRoute.route
 
     override val arguments: List<NamedNavArgument>
         get() = listOf() // pass any argument needed
